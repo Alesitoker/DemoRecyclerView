25632df83a9f1107555f1f5830e45247a8061a97
Alejandro <alesitonightray@gmail.com>
2018-11-07 18:10:40 +0100

    Mostrar estundiastes y eliminarlos al hacer click

--- app/src/main/java/es/iessaladillo/pedrojoya/demorecyclerview/data/local/Database.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/demorecyclerview/data/local/Database.java
@@ -1,11 +1,28 @@
 package es.iessaladillo.pedrojoya.demorecyclerview.data.local;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import es.iessaladillo.pedrojoya.demorecyclerview.data.local.model.Student;
+
 public class Database {
 
     // TODO: Define Database as a singleton.
 
-    // TODO: Define an ArrayList as students table.
+    private ArrayList<Student> students = new ArrayList<>(Arrays.asList(
+            new Student((long) 0, "Baldo", 23),
+            new Student((long) 1, "German", 28)
+    ));
 
-    // TODO: Add method to query students table.
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void deleteStudent(Student student) {
+        students.remove(student);
+    }
+
+// TODO: Add method to query students table.
 
 }
--- app/src/main/java/es/iessaladillo/pedrojoya/demorecyclerview/ui/main/MainActivityViewModelFactory.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/demorecyclerview/ui/main/MainActivityViewModelFactory.java
@@ -1,4 +1,21 @@
 package es.iessaladillo.pedrojoya.demorecyclerview.ui.main;
 
-// TODO: Make class implements ViewModelProvider.Factory
-public class MainActivityViewModelFactory { }
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import es.iessaladillo.pedrojoya.demorecyclerview.data.local.Database;
+
+public class MainActivityViewModelFactory implements ViewModelProvider.Factory {
+
+    private final Database database;
+
+    public MainActivityViewModelFactory(Database database) {
+        this.database = database;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return (T) new MainActivityViewModel(database);
+    }
+}
